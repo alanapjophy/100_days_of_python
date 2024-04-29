34a9162a7f9ef7190b81d475a3b2ead361c6fabb
Alana PJ <alanapjaws@gmail.com>
2024-04-29 22:28:34 +0530

    rtyh

--- day_007/hangman.py
+++ day_007/hangman.py
@@ -2,4 +2,10 @@ print("Welcome to HangMan!")
 import random
 word_list = ["apple","orange","mango"]
 chosen_word = random.choice(word_list)
-print(chosen_word)
+# print(chosen_word)
+guess = input("Guess a letter : ").lower()
+chosen)letter = len(chosen_word)
+display = []
+for _ in range(chosen_word):
+    display += "_"
+print(display)
