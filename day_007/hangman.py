199908a8a6e2589bb0e5bd6abded70c9246dfec9
Alana PJ <alanapjaws@gmail.com>
2024-04-29 22:10:44 +0530

    improving

---
+++ day_007/hangman.py
@@ -0,0 +1,15 @@
+print("Welcome to HangMan!")
+import random
+list = ["apple","orange","mango"]
+word = random.choice(list)
+guess = input("Guess a letter : ").lower()
+display = []
+for "_" in word:
+    print("_")
+end_of_game = False
+while not end_of_game:
+    for letter in word:
+        if guess == letter:
+            print("Right")
+        else:
+            print("Wrong")
