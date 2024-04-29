a0a8e25f3defe598804e95837bcb80ed8173bda1
Alana PJ <alanapjaws@gmail.com>
2024-04-29 22:25:32 +0530

    dgg

--- day_007/hangman.py
+++ day_007/hangman.py
@@ -1,21 +1,5 @@
 print("Welcome to HangMan!")
 import random
-list = ["apple","orange","mango"]
-word = random.choice(list)
-length = len(word)
-display = []
-for _ in range(length):
-    display += "_"
-guess = input("Guess a letter : ").lower()
-print(guess)
-
-
-# end_of_game = False
-# while not end_of_game:
-#     for letter in word:
-#         if guess == letter:
-#             print("Right")
-#         else:
-#             print("Wrong")
-# if end_of_game:
-#     print("true")
+word_list = ["apple","orange","mango"]
+chosen_word = random.choice(word_list)
+print(chosen_word)
