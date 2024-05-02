bf81d1c9ea499bd7b95f02dd430824bf2a5c1bba
Alana PJ <alanapjaws@gmail.com>
2024-05-02 11:53:10 +0530

    encrypt

--- day_007/hangman.py
+++ day_007/hangman.py
@@ -1,19 +1,39 @@
+# # Make a simple outline
+# print("Wlecome to HangMan")
+# import random
+# word_list = ["apple","orange","mango"]
+# chosen_word = random.choice(word_list).lower()
+# word_length = len(chosen_word)
+# display = []
+# for i in range(word_length):
+#     display += "_"
+# print(display)
+# guess = input("Guess a letter: ").lower()
+# for key in chosen_word:
+#     if guess == key:
+#         print("right")
+#     else:
+#         print("Wrong")
+
+
+
+# Make letters in list
 print("Wlecome to HangMan")
 import random
 word_list = ["apple","orange","mango"]
 chosen_word = random.choice(word_list).lower()
 word_length = len(chosen_word)
+end_of_game = false
 display = []
 for i in range(word_length):
     display += "_"
 print(display)
-guess = input("Guess a letter: ").lower()
-# print(guess)
-position = 0
-for key in chosen_word:
-    position += 1
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+    for key in chosen_word:
     if guess == key:
-        # print("right")
-        display[position] = guess
+        for i in range(word_length):
+
+        print("right")
     else:
         print("Wrong")
